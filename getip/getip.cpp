9698b3a87ce1a3dbcb677a1f9d9a5b75f83d4485
mengbin <mengbin@192.168.10.110>
2020-07-20 11:15:48 +0800

    OS X but not debug

--- getip/getip.cpp
+++ getip/getip.cpp
@@ -6,7 +6,66 @@ map<string, string> GetIP(map<string, string> &macs)
     ifconf cfg;
     char mac[32] = {0};
     char *ip = NULL;
+    int ret;
+#if __APPLE__
+    ifaddrs *ifap = new (ifaddrs);
+    if (ifap == NULL)
+    {
+        cout << strerror(errno) << endl;
+        goto END;
+    }
+    int mib[6];
+    size_t len;
+    ifaddrs *tmp;
+    mib[0] = CTL_NET;
+    mib[1] = AF_ROUTE;
+    mib[2] = 0;
+    mib[3] = AF_LINK;
+    mib[4] = NET_RT_IFLIST;
+    ret = getifaddrs(&ifap);
+    if (ret != 0)
+    {
+        cout << strerror(errno) << endl;
+        goto END;
+    }
+    tmp = ifap;
+    while (ifap->ifa_next)
+    {
+        struct sockaddr_in *addr = (struct sockaddr_in *)(ifap->ifa_addr);
+        if (addr->sin_family == AF_INET)
+        {
+            char *ip = inet_ntoa(addr->sin_addr);
+            mib[5] = if_nametoindex(ifap->ifa_name);
+            if (sysctl(mib, 6, NULL, &len, NULL, 0) < 0)
+            {
+                printf("sysctl 1 error\n");
+                continue;
+            }
+            char *macbuf = new char[len];
+            if (sysctl(mib, 6, macbuf, &len, NULL, 0) < 0)
+            {
+                cout << strerror(errno) << endl;
+                freeifaddrs(tmp);
+                goto END;
+            }
+            if_msghdr *ifm = (if_msghdr *)macbuf;
+            struct sockaddr_dl *sdl = (struct sockaddr_dl *)(ifm);
+            char *ptr = LLADDR(sdl);
+            sprintf(mac, "%02x:%02x:%02x:%02x:%02x:%02x",
+                    (unsigned char)ptr[0],
+                    (unsigned char)ptr[1],
+                    (unsigned char)ptr[2],
+                    (unsigned char)ptr[3],
+                    (unsigned char)ptr[4],
+                    (unsigned char)ptr[5]);
+            macs.insert(make_pair<string, string>(mac, ip));
+            delete[] macbuf;
+        }
 
+        ifap = ifap->ifa_next;
+    }
+    freeifaddrs(tmp);
+#else
     int fd = socket(AF_INET, SOCK_DGRAM, 0);
     if (fd < 0)
         return macs;
@@ -18,6 +77,7 @@ map<string, string> GetIP(map<string, string> &macs)
         int count = cfg.ifc_len / sizeof(ifreq);
         while (count-- > 0)
         {
+
             if (!ioctl(fd, SIOCGIFADDR, (char *)&networks[count]))
             {
                 ip = inet_ntoa(((struct sockaddr_in *)(&networks[count].ifr_ifru.ifru_addr))->sin_addr);
@@ -40,7 +100,8 @@ map<string, string> GetIP(map<string, string> &macs)
     {
         cout << "errno: " << errno << endl;
     }
-
     close(fd);
+#endif
+END:
     return macs;
 }
--- getip/getip.h
+++ getip/getip.h
@@ -7,9 +7,15 @@
 using namespace std;
 
 #include <arpa/inet.h>
+#include <ifaddrs.h>
 #include <net/if.h>
+#include <net/if_dl.h>
+#include <string.h>
+#include <sys/errno.h>
 #include <sys/ioctl.h>
 #include <sys/socket.h>
+#include <sys/sysctl.h>
+#include <sys/types.h>
 #include <unistd.h>
 
 map<string, string> GetIP(map<string, string> &macs);
